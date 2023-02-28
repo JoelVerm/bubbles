fce7ce43095b537a9a83da7744861147925836f0
Joël Vermeulen <98051242+JoelVerm@users.noreply.github.com>
2023-02-28 21:11:33 +0100

    various DB improvements

--- server/api/data.js
+++ server/api/data.js
@@ -4,17 +4,25 @@ import { db } from './db.js'
  * @param {String} query
  * @param {Record<string, unknown>} variables
  */
-const dbQuery = async (query, variables) =>
-    (await db.query(query, variables).catch(console.error))[0].result.map(e => {
-        e.id = e.id.split(':')[1]
-        return e
-    })
+const dbQuery = async (query, variables = undefined) =>
+    (await db.query(query, variables).catch(console.error))?.[0].result.map(
+        e => {
+            e.id = e.id.split(':')[1]
+            return e
+        }
+    )
 
 /**
- * @param {import('../../main.js').RunningRequest} req
+ *
+ * @param {{
+ *      type: 'add' | 'update' | 'get' | 'delete'| 'random' | 'query' | 'related',
+ *      content?:String,
+ *      tags?:Array<string>,
+ *      id?:String
+ * }} q
+ * @returns
  */
-export async function flami(req) {
-    let q = await req.getPostData()
+export async function query(q) {
     let note
     switch (q.type) {
         case 'add':
@@ -26,48 +34,67 @@ export async function flami(req) {
                 .catch(console.error)
             break
         case 'update':
-            note = (await db.select(`notes:${q.id}`).catch(console.error))[0]
-            await db
-                .update(`notes:${q.id}`, {
-                    ...note,
-                    content: q.content,
-                    tags: q.tags
+            note = (await db.select(`notes:${q.id}`).catch(console.error))?.[0]
+            if (!note)
+                return await query({
+                    ...q,
+                    type: 'add'
                 })
-                .catch(console.error)
+            if (q.content) note.content = q.content
+            if (q.tags) note.tags = q.tags
+            await db.update(`notes:${q.id}`, note).catch(console.error)
             break
         case 'get':
-            note = (await db.select(`notes:${q.id}`).catch(console.error))[0]
+            note = (await db.select(`notes:${q.id}`).catch(console.error))?.[0]
+            if (!note)
+                return {
+                    ERROR: `Unable to retrieve note with id ${q.id}`
+                }
             break
         case 'delete':
             await db.delete(`notes:${q.id}`).catch(console.error)
-            return []
+            return { SUCCESS: `Deleted node with id ${q.id}` }
+        case 'random':
+            return (
+                (
+                    await dbQuery('SELECT * FROM notes ORDER BY RAND() LIMIT 1')
+                )?.[0] ?? { ERROR: 'Unable to retrieve random note' }
+            )
         case 'query':
-            return await dbQuery(
-                `SELECT * FROM (SELECT *, count([${q.tags
-                    .map((t, i) => `tags ?~ $t${i}`)
-                    .join(
-                        ', '
-                    )}]) AS matches FROM notes ORDER BY RAND()) WHERE matches >= $minTagCount AND content ~ $content ORDER BY matches DESC LIMIT 50`,
-                {
-                    ...Object.fromEntries(q.tags.map((t, i) => [`t${i}`, t])),
-                    minTagCount: Math.round(q.tags.length / 2),
-                    content: q.content
-                }
+            return (
+                (await dbQuery(
+                    `SELECT * FROM (SELECT *, count([${q.tags
+                        .map((t, i) => `tags ?~ $t${i}`)
+                        .join(
+                            ', '
+                        )}]) AS matches FROM notes ORDER BY RAND()) WHERE matches >= $minTagCount AND content ~ $content ORDER BY matches DESC LIMIT 50`,
+                    {
+                        ...Object.fromEntries(
+                            q.tags.map((t, i) => [`t${i}`, t])
+                        ),
+                        minTagCount: Math.round(q.tags.length / 2),
+                        content: q.content
+                    }
+                )) ?? { ERROR: 'Unable to find matching search results' }
             )
         case 'related':
-            return await dbQuery(
-                `SELECT * FROM (SELECT *, count([${q.tags
-                    .map((t, i) => `tags ?~ $t${i}`)
-                    .join(
-                        ', '
-                    )}]) AS matches FROM notes ORDER BY RAND()) WHERE matches >= $minTagCount AND content ~ $content ORDER BY matches DESC LIMIT 50`,
-                {
-                    ...Object.fromEntries(q.tags.map((t, i) => [`t${i}`, t])),
-                    minTagCount: Math.round(q.tags.length / 2),
-                    content: q.content
-                }
+            return (
+                (await dbQuery(
+                    'SELECT *, array::intersect(tags, $noteTags) AS intersect FROM notes WHERE  tags ANYINSIDE $noteTags',
+                    {
+                        noteTags: q.tags
+                    }
+                )) ?? { ERROR: 'Unable to find related notes' }
             )
     }
     note.id = note.id.split(':')[1]
     if (note) return note
+}
+
+/**
+ * @param {import('../../main.js').RunningRequest} req
+ */
+export async function flami(req) {
+    let q = await req.getPostData()
+    return await query(q)
 }
