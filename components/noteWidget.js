695b12cbf55f2f71b00809c7ff24ecf371238494
JoelVerm <joel.vermeulen5@outlook.com>
2023-04-08 16:41:20 +0200

    unstupidified slicing sruff

--- components/noteWidget.js
+++ components/noteWidget.js
@@ -1,47 +1,23 @@
 const maxAllowedLines = 5
-export const page = (note, onNoteClick, tagComponent) => html`<div
-    class="noteWidget"
-    onclick=${onNoteClick}
->
-    <div class="content">
-        <img src
-            onerror=${e => {
-                const element = e.target.parentNode
-                const resizeHandler = () => {
-                    const width = element.offsetWidth
-                    const fontSize = Number(
-                        getComputedStyle(element).fontSize.slice(0, 2)
-                    )
-                    const charsPerLine = Math.floor((width / fontSize) * 2)
-                    let lineCount = 0
-                    let oldLineCount = 0
-                    let allowedLines = []
-                    for (let line of note.content.split('\n')) {
-                        oldLineCount = lineCount
-                        lineCount += Math.ceil(line.length / charsPerLine)
-                        if (lineCount > maxAllowedLines)
-                            line = line.slice(
-                                0,
-                                (maxAllowedLines - oldLineCount) * charsPerLine
-                            )
-                        allowedLines.push(line)
-                        if (lineCount > maxAllowedLines) break
-                        lineCount++
-                    }
-                    let text = allowedLines.join('\n')
-                    if (lineCount > maxAllowedLines) text += '\n...'
-                    element.innerText = text
-                }
-                addEventListener('resize', resizeHandler)
-                resizeHandler()
-            }}
-        ></img>
-        ${note.content
-            .split('\n')
-            .slice(0, 5)
-            .map((s, i, a) =>
-                i < a.length - 1 ? html`${s} <br /> ` : html`${s}`
-            )}${note.content.split('\n').length > 5 ? html`<br />...` : ''}
-    </div>
-    <div class="tags">${note.tags.map(t => tagComponent(t))}</div>
-</div>`
+const maxAllowedChars = 500
+export const page = (note, onNoteClick, tagComponent) => {
+    let content = note.content
+    let hasDots = false
+    if (content.length > maxAllowedChars) {
+        hasDots = true
+        content = content
+            .slice(0, maxAllowedChars)
+            .split(' ')
+            .slice(0, -1)
+            .join(' ')
+    }
+    let contentLines = content.split('\n')
+    if (contentLines.length > maxAllowedLines) {
+        hasDots = true
+        content = contentLines.slice(0, maxAllowedLines).join('\n')
+    }
+    return html`<div class="noteWidget" onclick=${onNoteClick}>
+        <div class="content">${content}${hasDots ? html`<br />...` : ''}</div>
+        <div class="tags">${note.tags.map(t => tagComponent(t))}</div>
+    </div>`
+}
