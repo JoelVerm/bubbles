44004c8bd9f54e343af4d02e08ef6d8e073d165a
JoelVerm <joel.vermeulen5@outlook.com>
2023-04-08 11:49:11 +0200

    good cropping finally

--- components/noteWidget.js
+++ components/noteWidget.js
@@ -1,8 +1,41 @@
+const maxAllowedLines = 5
 export const page = (note, onNoteClick, tagComponent) => html`<div
     class="noteWidget"
     onclick=${onNoteClick}
 >
     <div class="content">
+        <img src
+            onerror=${e => {
+                const element = e.target.parentNode
+                const resizeHandler = () => {
+                    const width = element.offsetWidth
+                    const fontSize = Number(
+                        getComputedStyle(element).fontSize.slice(0, 2)
+                    )
+                    const charsPerLine = Math.floor((width / fontSize) * 2)
+                    let lineCount = 0
+                    let oldLineCount = 0
+                    let allowedLines = []
+                    for (let line of note.content.split('\n')) {
+                        oldLineCount = lineCount
+                        lineCount += Math.ceil(line.length / charsPerLine)
+                        if (lineCount > maxAllowedLines)
+                            line = line.slice(
+                                0,
+                                (maxAllowedLines - oldLineCount) * charsPerLine
+                            )
+                        allowedLines.push(line)
+                        if (lineCount > maxAllowedLines) break
+                        lineCount++
+                    }
+                    let text = allowedLines.join('\n')
+                    if (lineCount > maxAllowedLines) text += '\n...'
+                    element.innerText = text
+                }
+                addEventListener('resize', resizeHandler)
+                resizeHandler()
+            }}
+        ></img>
         ${note.content
             .split('\n')
             .slice(0, 5)
